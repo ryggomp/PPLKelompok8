9242d3709d04603eaec1704afd112f20f3934661
Arya Dayusma Mundi <aryadym29@gmail.com>
2025-03-26 09:29:34 +0700

    Menambahkan Fitur Registrasi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
 
 Route::get('/', function () {
     return view('welcome');
