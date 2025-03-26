2118cf13fcf157cda6e3750c00e811ef052df203
ryggomp <ryggomp7f@gmail.com>
2025-03-26 08:20:10 +0700

    feat(PBI-002A): implement edit profile route, controller, model, view, and update migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->integer('points')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
